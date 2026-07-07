f0cf4ccf69d868d06e610db0f2fc1012102d12ef
Streetsraker <kim842058@gmail.com>
2026-07-07 21:10:26 +0300

    fix: tests

--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
+from jwt import PyJWTError, decode
 from sqlalchemy.orm import Session
 from backend.src.database import get_db
 from backend.src.models import User, UserRole
@@ -17,12 +17,12 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
-    except JWTError:
+    except PyJWTError:
         raise credentials_exception
 
     user = db.query(User).filter(User.username == token_data.username).first()
